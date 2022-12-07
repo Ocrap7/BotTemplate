a43886daddf57fabf2c0e4c3ddfb400ec1c8b78c
Oliver Clarke <oliverhenryc@gmail.com>
2022-12-07 12:15:34 -0600

    Base

---
+++ dist/commands/help.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.command = void 0;
+exports.command = new SlashCommandBuilder()
+    .setName('stereo')
+    .setDescription('Music stereo');
+//# sourceMappingURL=help.js.map
